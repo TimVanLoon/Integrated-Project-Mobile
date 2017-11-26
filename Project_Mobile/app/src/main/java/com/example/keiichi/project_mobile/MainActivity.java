0e2109f5d51d91ad9f74cd4e94f1cc518f2bfa46
KevinBeckwee <medabots4@hotmail.com>
2017-11-26 23:14:21 +0100

    Api calls functionaliteit toegevoegd
    Nu mogelijk om naar Microsoft Graph calls te sturen, werkt en mails
    ophalen lukt ook.

--- Project_Mobile/app/src/main/java/com/example/keiichi/project_mobile/MainActivity.java
+++ Project_Mobile/app/src/main/java/com/example/keiichi/project_mobile/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.keiichi.project_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.microsoft.identity.client.*;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TEST_MESSAGE = "heyboo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view){
+        Intent intent = new Intent(this, DisplayMessageActvity.class);
+        EditText editText = (EditText) findViewById(R.id.editText3);
+        String message = editText.getText().toString();
+        intent.putExtra(TEST_MESSAGE, message);
+        startActivity(intent);
+
     }
 }
