f4ef38b6dcae7ba957fd5bbf9b7ae502d60fee1a
KevinBeckwee <medabots4@hotmail.com>
2017-12-12 11:48:42 +0100

    backup
    backu

--- Project_Mobile/app/src/main/java/com/example/keiichi/project_mobile/MailAdapter.java
+++ Project_Mobile/app/src/main/java/com/example/keiichi/project_mobile/MailAdapter.java
@@ -1,9 +1,11 @@
 package com.example.keiichi.project_mobile;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -26,11 +28,36 @@ public class MailAdapter extends RecyclerView.Adapter<MailAdapter.MyViewHolder> 
 
 
     @Override
-    public void onBindViewHolder(MailAdapter.MyViewHolder holder, int position) {
+    public void onBindViewHolder(final MailAdapter.MyViewHolder holder, int position) {
         try {
-            JSONObject mailObject = jsonArray.getJSONObject(position);
+            final JSONObject mailObject = jsonArray.getJSONObject(position);
+            mailObject.put("selected", false);
             holder.bodyPreview.setText(mailObject.getString("bodyPreview"));
             holder.subject.setText(mailObject.getString("subject"));
+            if (mailObject.getBoolean("selected")) {
+
+                holder.check.setVisibility(View.VISIBLE);
+            }else {
+                holder.check.setVisibility(View.INVISIBLE);
+            }
+
+            /*holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    try {
+                        mailObject.put("selected",!mailObject.getBoolean("selected"));
+                        if (mailObject.getBoolean("selected")) {
+
+                            holder.check.setVisibility(View.VISIBLE);
+                        }else {
+                            holder.check.setVisibility(View.INVISIBLE);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    return false;
+                }
+            });*/
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -45,10 +72,12 @@ public class MailAdapter extends RecyclerView.Adapter<MailAdapter.MyViewHolder> 
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         public TextView subject,bodyPreview;
+        public ImageView check;
         public MyViewHolder(View view){
             super(view);
             this.subject = view.findViewById(R.id.previewBody);
             this.bodyPreview = view.findViewById(R.id.header);
+            this.check = view.findViewById(R.id.CheckImg);
         }
 
     }
