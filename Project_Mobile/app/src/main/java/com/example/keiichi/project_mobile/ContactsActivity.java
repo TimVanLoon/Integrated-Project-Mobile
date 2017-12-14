b7af8e7bbeed5d5bd4810d0b870a460da1870efe
TimVanLoon <timervan@hotmail.com>
2017-12-14 23:32:28 +0100

    Revert

---
+++ Project_Mobile/app/src/main/java/com/example/keiichi/project_mobile/ContactsActivity.java
@@ -0,0 +1,230 @@
+package com.example.keiichi.project_mobile;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    BottomNavigationView mBottomNav;
+
+    private DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    private ListView contactsListView;
+
+    SearchView searchView;
+
+    ContactAdapter contactAdapter;
+
+    private String accessToken;
+
+    final static String MSGRAPH_URL = "https://graph.microsoft.com/v1.0/me/contacts";
+
+    /* UI & Debugging Variables */
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, myToolbar, R.string.drawer_open,
+                R.string.drawer_close);
+
+        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+
+        contactsListView = (ListView) findViewById(R.id.contactsListView);
+
+        accessToken = getIntent().getStringExtra("AccessToken");
+        callGraphAPI();
+
+        mBottomNav = (BottomNavigationView) findViewById(R.id.navigation);
+
+        Menu menu = mBottomNav.getMenu();
+        MenuItem menuItem = menu.getItem(2);
+        menuItem.setChecked(true);
+
+        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId()) {
+
+                    case R.id.action_calendar:
+                        Intent intentCalendar = new Intent(ContactsActivity.this, CalendarActivity.class);
+                        intentCalendar.putExtra("AccessToken", accessToken);
+                        startActivity(intentCalendar);
+                        break;
+                    case R.id.action_mail:
+                        Intent intentMail = new Intent(ContactsActivity.this, ListMailsActvity.class);
+                        intentMail.putExtra("AccessToken", accessToken);
+                        startActivity(intentMail);
+                        break;
+                    case R.id.action_user:
+
+                        break;
+
+                }
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_action_bar_items, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                //contactAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch(item.getItemId()){
+
+
+
+            case R.id.action_add:
+
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /* Use Volley to make an HTTP request to the /me endpoint from MS Graph using an access token */
+    private void callGraphAPI() {
+        Log.d(TAG, "Starting volley request to graph");
+        Log.d(TAG, accessToken);
+
+    /* Make sure we have a token to send to graph */
+        if (accessToken == null) {
+            return;
+        }
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JSONObject parameters = new JSONObject();
+
+        try {
+            parameters.put("key", "value");
+        } catch (Exception e) {
+            Log.d(TAG, "Failed to put parameters: " + e.toString());
+        }
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, MSGRAPH_URL,
+                parameters, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+            /* Successfully called graph, process data and send to UI */
+                Log.d(TAG, "Response: " + response.toString());
+
+                try {
+                    updateGraphUI(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "Error: " + error.toString());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + accessToken);
+                return headers;
+            }
+        };
+
+        Log.d(TAG, "Adding HTTP GET to Queue, Request: " + request.toString());
+
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                3000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        queue.add(request);
+    }
+
+    /* Sets the Graph response */
+    private void updateGraphUI(JSONObject graphResponse) throws JSONException {
+
+        // Test de response
+        System.out.println(graphResponse);
+        JSONArray contactsJsonArray = null;
+
+        // Haal de contacten binnen
+        try {
+            contactsJsonArray = (JSONArray) graphResponse.get("value");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        assert contactsJsonArray != null;
+
+        contactAdapter = new ContactAdapter(this, contactsJsonArray);
+        contactsListView.setAdapter(contactAdapter);
+
+    }
+}
