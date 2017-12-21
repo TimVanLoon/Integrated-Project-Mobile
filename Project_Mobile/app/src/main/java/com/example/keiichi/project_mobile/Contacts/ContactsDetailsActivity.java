7a0bfb42356b4d494ddf3f1c9105b0672e66184c
TimVanLoon <timervan@hotmail.com>
2017-12-21 12:35:54 +0100

    Action bar action details

--- Project_Mobile/app/src/main/java/com/example/keiichi/project_mobile/Contacts/ContactsDetailsActivity.java
+++ Project_Mobile/app/src/main/java/com/example/keiichi/project_mobile/Contacts/ContactsDetailsActivity.java
@@ -8,6 +8,8 @@ import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
@@ -150,6 +152,16 @@ public class ContactsDetailsActivity extends AppCompatActivity {
         headerDisplayName.setText(displayName);
         email.setText(userEmail);
         userPhone.setText(phoneNumber);
+    }
+
+    // VOEG ICONS TOE AAN DE ACTION BAR
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_navigation, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     // METHODE VOOR DE CLICKABLE ICOONTJES IN DE ACTION BAR
