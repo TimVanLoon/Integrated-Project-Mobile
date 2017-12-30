ea251cd6e62d6fcc1750dfc5aa7d8269b85cc956
Tim Van Loon <s089249@ap.be>
2017-12-31 00:22:27 +0100

    Events activity verbeterd

--- Project_Mobile/app/src/main/java/com/example/keiichi/project_mobile/Calendar/ListEventsActivity.java
+++ Project_Mobile/app/src/main/java/com/example/keiichi/project_mobile/Calendar/ListEventsActivity.java
@@ -3,8 +3,15 @@ package com.example.keiichi.project_mobile.Calendar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.SearchView;
 
+import com.example.keiichi.project_mobile.Contacts.AddContactActivity;
+import com.example.keiichi.project_mobile.Contacts.ContactsActivity;
 import com.example.keiichi.project_mobile.DAL.POJOs.Event;
 import com.example.keiichi.project_mobile.R;
 
@@ -16,6 +23,11 @@ import java.util.List;
 
 public class ListEventsActivity extends AppCompatActivity {
 
+    private String accessToken;
+    private String userName;
+    private String userEmail;
+    private Toolbar myToolbar;
+    private SearchView searchView;
     private ListView eventsListView;
     private List<Event> events = new ArrayList<>();
 
@@ -24,7 +36,11 @@ public class ListEventsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_events);
 
+
+        myToolbar = (Toolbar) findViewById(R.id.toolbar);
         eventsListView = (ListView) findViewById(R.id.eventsListView);
+
+        setSupportActionBar(myToolbar);
 
         Intent intent = getIntent();
         String eventsArrayInString = intent.getStringExtra("EventsArray");
@@ -45,5 +61,60 @@ public class ListEventsActivity extends AppCompatActivity {
         eventsListView.setAdapter(eventAdapter);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_action_bar_items_contacts, menu);
+        MenuItem addItem = menu.findItem(R.id.action_add);
+
+
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setQueryHint("Search calendar...");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+
+                return true;
+            }
+
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch(item.getItemId()){
+
+
+
+            case R.id.action_add:
+                Intent intentAddEvent = new Intent(ListEventsActivity.this, AddEventActivity.class);
+                intentAddEvent.putExtra("AccessToken", accessToken);
+                intentAddEvent.putExtra("userName", userName);
+                intentAddEvent.putExtra("userEmail", userEmail);
+
+                startActivity(intentAddEvent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
